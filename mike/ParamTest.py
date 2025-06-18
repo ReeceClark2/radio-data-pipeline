e9248956b4a0738ba7cd05d844f9bc3e9e5be23b
AnshMuthiyan <muthiyan@unc.edu>
2025-06-18 10:12:53 -0400

    Child now takes raw data cube, rather than the data cube from MIKE

--- mike/ParamTest.py
+++ mike/ParamTest.py
@@ -16,75 +16,76 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    fits_folder = "TrackingHighRes"
+    fits_folder = "EpicONoFFHiRes"
     fits_files = [f for f in os.listdir(fits_folder) if f.endswith(".fits")]
 
-    # for fits_filename in fits_files:
-    #     file_path = os.path.join(fits_folder, fits_filename)
+    for fits_filename in fits_files:
+        file_path = os.path.join(fits_folder, fits_filename)
 
-    #     file = Mike(file_path)
-    #     cal_file = Mike("TrackingHighRes/0136483.fits")
+        file = Mike(file_path)
+        cal_file = Mike("TrackingHighRes/0136483.fits")
             
-    #     v = Val(file)
-    #     v.validate_primary_header()
-    #     v.validate_data()
+        v = Val(file)
+        v.validate_primary_header()
+        v.validate_data()
 
-    #     s = Sort(file)
-    #     s.sort()
+        s = Sort(file)
+        s.sort()
 
-    #     # w = Weather(file)
-    #     # w.weather_correction()
+        # w = Weather(file)
+        # w.weather_correction()
 
-    #     gc = Gain_Cal(file)
-    #     gc.gain_cal()
+        gc = Gain_Cal(file)
+        gc.gain_cal()
 
 
-    #     v = Val(cal_file)
-    #     v.validate_primary_header()
-    #     v.validate_data()
+        v = Val(cal_file)
+        v.validate_primary_header()
+        v.validate_data()
 
-    #     s = Sort(cal_file)
-    #     s.sort()
+        s = Sort(cal_file)
+        s.sort()
 
-    #     w = Weather(cal_file)
-    #     w.weather_correction()
+        w = Weather(cal_file)
+        w.weather_correction()
 
-    #     gc = Gain_Cal(cal_file)
-    #     gc.gain_cal()
+        gc = Gain_Cal(cal_file)
+        gc.gain_cal()
 
-    #     # fc = Flux_Cal(file, cal_file)
-    #     # fc.flux_cal()
+        # fc = Flux_Cal(file, cal_file)
+        # fc.flux_cal()
 
-    #     spec = Spectrum(file)
-    #     spec.make_spec()
+        spec = Spectrum(file)
+        spec.make_spec()
 
-    #     start_time = time.time()
-    #     print(f"Processing file: {fits_filename}")
-    #     keep_indices = [[7,13], [15,21]]  # Specify the indices you want to keep
-    #     feed= [0,1]  # Specify the feeds you want to keep
-    #     if keep_indices != []:
-    #         child = Sully(file, keep_indices, "continuum", "cut", feed)
-    #         child.user_cuts([[1300, 1400], [1402, 1404], [1412, 1420]], "spectrum", "keep", feed)
-    #         child.specMaker()
-    #         gcChild = Gain_Cal(child)
-    #         gcChild.gain_cal()
+        start_time = time.time()
+        print(f"Processing file: {fits_filename}")
+        keep_indices = [[7,13], [15,21]]  # Specify the indices you want to keep
+        feed= [0,1]  # Specify the feeds you want to keep
+        if keep_indices != []:
+            child = Sully(file, file_path, keep_indices, "continuum", "cut", feed)
+            child.user_cuts([[1300, 1400], [1402, 1404], [1412, 1420]], "spectrum", "keep", feed)
+            child.specMaker()
+            gcChild = Gain_Cal(child)
+            gcChild.gain_cal()
 
-    #         # print(file.data[0]["DATA"].shape)
-    #         # print(child.data[0]["DATA"].shape)
+            # print(file.data[0]["DATA"].shape)
+            # print(child.data[0]["DATA"].shape)
 
-    #     end_time = time.time()
-    #     elapsed_time = end_time - start_time
-    #     print(f"Time taken for this file: {elapsed_time:.4f} seconds")
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Time taken for this file: {elapsed_time:.4f} seconds")
 
-    #     # Store elapsed times for averaging
-    #     if 'elapsed_times' not in locals():
-    #         elapsed_times = []
-    #     elapsed_times.append(elapsed_time)
-    # print(f"Average time taken so far: {np.mean(elapsed_times):.4f} seconds")
+        # Store elapsed times for averaging
+        if 'elapsed_times' not in locals():
+            elapsed_times = []
+        elapsed_times.append(elapsed_time)
+    print(f"Average time taken so far: {np.mean(elapsed_times):.4f} seconds")
 
 
     fig, axs = plt.subplots(2, 2, figsize=(12, 8))
-    file = Mike("TrackingHighRes/0136764.fits")
+    file_path = "TrackingHighRes/0136764.fits"
+    file = Mike(file_path)
     cal_file = Mike("TrackingHighRes/0136483.fits")
 
 
@@ -122,7 +123,7 @@ if __name__ == "__main__":
     keep_indices = [[7,13], [15,21]]  # Specify the indices you want to keep
     feed= [0,1]  # Specify the feeds you want to keep
     if keep_indices != []:
-        child = Sully(file, keep_indices, "continuum", "cut", feed)
+        child = Sully(file, file_path, keep_indices, "continuum", "cut", feed)
         child.user_cuts([[1300, 1400], [1402, 1404], [1412, 1420]], "spectrum", "keep", feed)
         child.specMaker()
         print('hi')
