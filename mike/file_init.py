d3f4c9124d22a3b7a82ac11621b579518d40a482
ReeceClark2 <reececlark55@gmail.com>
2025-06-20 12:05:37 -0400

    Organize imports and  rename Mike ;-;

--- mike/file_init.py
+++ mike/file_init.py
@@ -1,13 +1,14 @@
+# Third-party libraries
+import numpy as np
 from astropy.io import fits
 from astropy.table import Table
+
+# Local application imports
 from file_exception import MyException
-import numpy as np
-import matplotlib
-matplotlib.use("Agg")
-import matplotlib.pyplot as plt
+from logger import Log_Collector
 
 
-class Mike:
+class Radio_File:
     def __init__(self, file_path):
         '''
         Initialize binary fits cube by reading its header and data from a given file.
@@ -40,16 +41,19 @@ class Mike:
             with fits.open(file_path) as hdul:
                 self.header = hdul[0].header
                 self.data = Table(hdul[1].data)
+            self.file.logger.info(f"Error reading FITS file: {e}")
         except Exception as e:
+            self.file.logger.error(f"Error reading FITS file: {e}")
             raise MyException(f"Error reading FITS file: {e}")
         
-        self.missing_values = []
         self.file_path = file_path
+        self.logger = Log_Collector(name=f"logger_{file_path}")
 
         self.validated_header = False
         self.validated_data = False
-        self.gain_calibrated = False
-        self.flux_calibrated = False
+
+        self.gain_calibrated = []
+        self.flux_calibrated = []
 
         self.labels = []
         self.data_indicies = []
@@ -67,9 +71,9 @@ if __name__ == "__main__":
     Test function to test uploaded file.
     '''
 
-    file = Mike("C:/Users/starb/Downloads/0136870.fits")
+    file = Radio_File("C:/Users/starb/Downloads/0136645.fits")
 
     np.set_printoptions(threshold=100000)
 
     print(repr(file.header))
-    # print(repr(file.data['OBSMODE']))
+    print(repr(file.data['TAMBIENT']))
