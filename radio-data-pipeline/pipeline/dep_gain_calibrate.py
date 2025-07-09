144c927b69048d00bd5103b850cc977b0be217e0
ReeceClark2 <reececlark55@gmail.com>
2025-07-09 14:45:17 -0400

    Modify

--- radio-data-pipeline/pipeline/dep_gain_calibrate.py
+++ radio-data-pipeline/pipeline/dep_gain_calibrate.py
@@ -23,8 +23,7 @@ class Gain_Calibrate:
             with fits.open(filepath) as hdul:
                 self.header = hdul[0].header
                 self.data = Table(hdul[1].data)
-                num_points = len(self.data[(self.data['CALSTATE'] == 0) & (self.data['SWPVALID'] == 0)])
-                print(num_points)
+            
             # TODO add logging here
         except Exception as e:
             # TODO add logging here
@@ -68,6 +67,7 @@ class Gain_Calibrate:
             if pre_cal_complete and i['SWPVALID'] == 0 and i['CALSTATE'] == 1:
                 break
 
+
         self.indices = [data_start_ind, post_cal_start_ind]
 
         if self.header['OBSMODE'] == 'onoff':
@@ -97,7 +97,7 @@ class Gain_Calibrate:
         '''
         Perform functional Robust Chauvenet Rejection on a given dataset.
         '''
-        
+
         x = array[0]
         x -= np.average(x)
 
@@ -168,12 +168,13 @@ class Gain_Calibrate:
 
         # Partition into pre and post ranges
         pre_data = [row for row in cached_data if row['idx'] < self.indices[0]]
+
         data_array = [row for row in cached_data if self.indices[0] < row['idx'] < self.indices[1]]
         post_data = [row for row in cached_data if row['idx'] > self.indices[1]]
 
-        def extract(data_subset, swpvalid_val, calstate_val, i, j, h):
+        def extract(data_subset, swpvalid_val, calstate_val, i, j):
             rows = [
-                ((row['time'] - t0).sec, row['DATA'][h])
+                ((row['time'] - t0).sec, np.mean(row['DATA']))
                 for row in data_subset
                 if row['SWPVALID'] == swpvalid_val and
                 row['CALSTATE'] == calstate_val and
@@ -187,113 +188,121 @@ class Gain_Calibrate:
 
         pre_cal_deltas = []
         post_cal_deltas = []
+        pre_cal_uncs = []
+        post_cal_uncs = []
 
         for i in ifnums:
             for j in plnums:
+                data = extract(data_array, 1, 0, i, j)
 
-                
+                pre_cal_delta = None
+                pre_cal_unc = None
+                try:
+                    # Extract pre arrays
+                    pre_on_times, pre_on_vals = extract(pre_data, 0, 1, i, j)
+                    pre_off_times, pre_off_vals = extract(pre_data, 0, 0, i, j)
 
-                for h in range(first_len):
-                    data = extract(data_array, 1, 0, i, j, h)
-
-                    try:
-                        # Extract pre arrays
-                        pre_on_times, pre_on_vals = extract(pre_data, 0, 1, i, j, h)
-                        pre_off_times, pre_off_vals = extract(pre_data, 0, 0, i, j, h)
-
-                        if len(pre_on_times) == 0 or len(pre_off_times) == 0:
-                            continue
-
-                        pre_on_params, pre_on_unc = self.rcr((pre_on_times, pre_on_vals))
-                        pre_off_params, pre_off_unc = self.rcr((pre_off_times, pre_off_vals))
-
-                        # Midpoint time
-                        pre_cal_time = np.mean([np.mean(pre_on_times), np.mean(pre_off_times)])
-                        pre_cal_time = np.clip(pre_cal_time,
-                                    (pre_on_times[0] + pre_off_times[-1]) / 2,
-                                    (pre_on_times[-1] + pre_off_times[0]) / 2)
-
-                        pre_cal_delta = np.abs(
-                            (pre_on_params[1] * (pre_cal_time - np.mean(pre_on_times)) + pre_on_params[0]) -
-                            (pre_off_params[1] * (pre_cal_time - np.mean(pre_off_times)) + pre_off_params[0])
-                        )
-                        pre_cal_unc = np.sqrt(
-                            pre_on_unc[0]**2 + pre_off_unc[0]**2 +
-                            (pre_on_unc[1] * pre_cal_time)**2 + (pre_off_unc[1] * pre_cal_time)**2
-                        )
-
-                        pre_cal_deltas.append(pre_cal_delta)
-                    except:
-                        pre_cal_delta = None
-                        pre_cal_unc = None
-
-
-                    try:
-                        # Extract post arrays
-                        post_on_times, post_on_vals = extract(post_data, 0, 1, i, j, h)
-                        post_off_times, post_off_vals = extract(post_data, 0, 0, i, j, h)
-
-                        if len(post_on_times) == 0 or len(post_off_times) == 0:
-                            continue
-
-                        post_on_params, post_on_unc = self.rcr((post_on_times, post_on_vals))
-                        post_off_params, post_off_unc = self.rcr((post_off_times, post_off_vals))
-
-                        post_cal_time = np.mean([np.mean(post_on_times), np.mean(post_off_times)])
-                        post_cal_time = np.clip(post_cal_time,
-                                    (post_on_times[0] + post_off_times[-1]) / 2,
-                                    (post_on_times[-1] + post_off_times[0]) / 2)
-
-                        post_cal_delta = np.abs(
-                            (post_on_params[1] * (post_cal_time - np.mean(post_on_times)) + post_on_params[0]) -
-                            (post_off_params[1] * (post_cal_time - np.mean(post_off_times)) + post_off_params[0])
-                        )
-                        post_cal_unc = np.sqrt(
-                            post_on_unc[0]**2 + post_off_unc[0]**2 +
-                            (post_on_unc[1] * post_cal_time)**2 + (post_off_unc[1] * post_cal_time)**2
-                        )
-
-                        post_cal_deltas.append(post_cal_delta)
-                    except:
-                        pre_cal_delta = None
-                        pre_cal_unc = None
-
+                    if len(pre_on_times) == 0 or len(pre_off_times) == 0:
+                        pre_cal_deltas.append(None)
+                        pre_cal_uncs.append(None)
                     
-                for h in range(first_len):
-                    if pre_cal_deltas[h] and post_cal_deltas[h]:
-                        pre_cal_delta = 6 #average_neighbors(pre_cal_deltas, h, 5)
-                        post_cal_delta = 6 # average_neighbors(post_cal_deltas, h, 5)
+                    pre_on_params, pre_on_unc = self.rcr([pre_on_times, pre_on_vals])
+                    pre_off_params, pre_off_unc = self.rcr([pre_off_times, pre_off_vals])
+                    # Midpoint time
+                    pre_cal_time = np.mean([np.mean(pre_on_times), np.mean(pre_off_times)])
+                    pre_cal_time = np.clip(pre_cal_time,
+                                (pre_on_times[0] + pre_off_times[-1]) / 2,
+                                (pre_on_times[-1] + pre_off_times[0]) / 2)
 
-                        z_value = abs(pre_cal_delta - post_cal_delta) / np.sqrt(pre_cal_unc**2 + post_cal_unc**2)
+                    pre_cal_delta = np.abs(
+                        (pre_on_params[1] * (pre_cal_time - np.mean(pre_on_times)) + pre_on_params[0]) -
+                        (pre_off_params[1] * (pre_cal_time - np.mean(pre_off_times)) + pre_off_params[0])
+                    )
+                    pre_cal_unc = np.sqrt(
+                        pre_on_unc[0]**2 + pre_off_unc[0]**2 +
+                        (pre_on_unc[1] * pre_cal_time)**2 + (pre_off_unc[1] * pre_cal_time)**2
+                    )
 
-                        if z_value < 0.6745:
-                            weights =  np.array([1 / pre_cal_unc, 1 / post_cal_unc])
-                            delta = np.average([pre_cal_delta, post_cal_delta], weights=weights)
-
-                            self.data['DATA'][:, h] /= delta
-                            print('z value not used')
-
-                        else:
-                            for ind, _ in enumerate(data):
-                                delta = pre_cal_delta + (post_cal_delta - pre_cal_delta) * (data[0][ind] - pre_cal_time) / (post_cal_time - pre_cal_time)
-                                
-                                self.data['DATA'][ind, h] /= delta
-                                print('z value used')
+                    pre_cal_deltas.append(pre_cal_delta)
+                    pre_cal_uncs.append(pre_cal_unc)
+                except:
+                    pre_cal_deltas.append(None)
+                    pre_cal_uncs.append(None)
 
 
-                    print(f"IFNUM={i}, PLNUM={j}, CH={h} → pre={pre_cal_delta}±{pre_cal_unc}, post={post_cal_delta}±{post_cal_unc}")
+                post_cal_delta = None
+                post_cal_unc = None
+                try:
+                    # Extract post arrays
+                    post_on_times, post_on_vals = extract(post_data, 0, 1, i, j)
+                    post_off_times, post_off_vals = extract(post_data, 0, 0, i, j)
+                    
+                    if len(post_on_times) == 0 or len(post_off_times) == 0:
+                        post_cal_deltas.append(None)
+                        post_cal_uncs.append(None)
 
-        print(np.median(pre_cal_deltas), np.median(post_cal_deltas))
-        
+                    post_on_params, post_on_unc = self.rcr((post_on_times, post_on_vals))
+                    post_off_params, post_off_unc = self.rcr((post_off_times, post_off_vals))
+
+                    post_cal_time = np.mean([np.mean(post_on_times), np.mean(post_off_times)])
+                    post_cal_time = np.clip(post_cal_time,
+                                (post_on_times[0] + post_off_times[-1]) / 2,
+                                (post_on_times[-1] + post_off_times[0]) / 2)
+
+                    post_cal_delta = np.abs(
+                        (post_on_params[1] * (post_cal_time - np.mean(post_on_times)) + post_on_params[0]) -
+                        (post_off_params[1] * (post_cal_time - np.mean(post_off_times)) + post_off_params[0])
+                    )
+                    post_cal_unc = np.sqrt(
+                        post_on_unc[0]**2 + post_off_unc[0]**2 +
+                        (post_on_unc[1] * post_cal_time)**2 + (post_off_unc[1] * post_cal_time)**2
+                    )
+
+                    post_cal_deltas.append(post_cal_delta)
+                    post_cal_uncs.append(post_cal_unc)
+                except:
+                    post_cal_deltas.append(None)
+                    post_cal_uncs.append(None)
+
             
+                if pre_cal_delta and post_cal_delta:
+                    print("Using pre and post calibration!")
+                    # pre_cal_delta = average_neighbors(pre_cal_deltas, h, 5)
+                    # post_cal_delta = average_neighbors(post_cal_deltas, h, 5)
 
-            # Now 'intensities' contains the i-th value across all rows
+                    z_value = abs(pre_cal_delta - post_cal_delta) / np.sqrt(pre_cal_unc**2 + post_cal_unc**2)
 
+                    if z_value < 0.6745:
+                        weights =  np.array([1 / pre_cal_unc, 1 / post_cal_unc])
+                        delta = np.average([pre_cal_delta, post_cal_delta], weights=weights)
 
+                        self.data['DATA'][:] /= delta
+                        continue
 
-    # def 
+                    else:
+                        for ind, _ in enumerate(data):
+                            delta = pre_cal_delta + (post_cal_delta - pre_cal_delta) * (data[0][ind] - pre_cal_time) / (post_cal_time - pre_cal_time)
+                            
+                            self.data['DATA'][ind] /= delta
+                            continue
 
+                elif pre_cal_delta and not post_cal_delta:
+                    print("Using pre calibration only!")
+                    
+                    self.data['DATA'][:] /= pre_cal_delta
+                    continue
 
+                elif not pre_cal_delta and post_cal_delta:
+                    print("Using post calibration only!")
+                    
+                    self.data['DATA'][:] /= post_cal_delta
+                    continue
+
+                print("No calibration found!")
+
+        # print(np.median(pre_cal_deltas), np.median(post_cal_deltas))
+        
+        
 
     def save(self, output_path=None):
         if output_path is None:
@@ -322,7 +331,7 @@ class Gain_Calibrate:
 
 
 if __name__ == "__main__":
-    filepath = "C:/Users/starb/Downloads/0136376.fits"
+    filepath = "C:/Users/starb/Downloads/0136868.fits"
     file = Gain_Calibrate(filepath)
 
     file.gain_calibrate()
